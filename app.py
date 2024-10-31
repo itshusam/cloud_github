6efda267f1da5da95e530741b85437a7676a3b95
itshusam <husam.attarabeen@gmail.com>
2024-10-31 17:01:10 -0500

    commit

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+sums = []
 
 @app.route('/sum', methods=['POST'])
-def sum():
-    data = request.get_json()
-    num1 = data['num1']
-    num2 = data['num2']
-    result = num1+num2
+def add_sum():
+    data = request.json
+    result = data.get('result')
+    sums.append(result)
+    return jsonify({'message': 'Sum added!', 'result': result}), 201
 
-    return jsonify({'result': result})
+@app.route('/sum', methods=['GET'])
+def get_all_sums():
+    return jsonify(sums), 200
 
+@app.route('/sum/result/<int:result>', methods=['GET'])
+def get_sums_by_result(result):
+    filtered_sums = [s for s in sums if s == result]
+    return jsonify(filtered_sums), 200
 
+if __name__ == '__main__':
+    app.run(debug=True)
